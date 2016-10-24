fe5c3fd15ece9ba263762b5cdc1d414872884938
Travis-CI <kanishk.kunal@hotmail.com>
2016-10-24 18:52:54 +0000

    Updates

--- image-cropper/scripts/image-cropper.js
+++ image-cropper/scripts/image-cropper.js
@@ -1,17 +1,22 @@
 /** Image Cropper Logic **/
 var cropper = null;
+var filename = "download";
 
-document.getElementById("select-all").addEventListener('click', select_all);
+function el(id){return document.getElementById(id);} // Get elem by ID
 
-document.getElementById("jpg-download").addEventListener('click', download_jpg);
-document.getElementById("png-download").addEventListener('click', download_png);
+el("select-all").addEventListener('click', select_all);
 
-var target = document.getElementById("drop-target");
+el("jpg-download").addEventListener('click', download_jpg);
+el("png-download").addEventListener('click', download_png);
+
+var target = el("drop-target");
 target.addEventListener("dragover", function(e){e.preventDefault();}, true);
 target.addEventListener("drop", function(e){
 	e.preventDefault();
 	loadImage(e.dataTransfer.files[0]);
 }, true);
+
+el("fileUpload").addEventListener("change", readImage, false);
 
 var rad = document.aspectform.aspectratio;
 for(var i = 0; i < rad.length; i++) {
@@ -34,12 +39,22 @@ for(var i = 0; i < rad.length; i++) {
     };
 }
 
+// Read image from input field
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        loadImage(this.files[0]);
+    }
+}
+
 function loadImage(src){
 	//	Prevent any non-image file type from being read.
 	if(!src.type.match(/image.*/)){
 		alert("The dropped file is not an image: ", src.type);
 		return;
 	}
+  el("load-prompt").innerHTML = "Loading...";
+
+  filename = src.name;
 
 	//	Create our FileReader and run the results through the render function.
 	var reader = new FileReader();
@@ -49,25 +64,36 @@ function loadImage(src){
 	reader.readAsDataURL(src);
 }
 function render(src){
-	var image =   document.getElementById("myimage");
+	var image =   el("myimage");
 	image.onload = function(){
-		var scale = 1;
-		if(image.naturalWidth > 800) {
-			scale = 800.0/image.naturalWidth;
-			image.width = 800;
-		}
+    // hide click target
+    el("click-target").style.display = 'none';
 
-    cropper = new Cropper(document.getElementById('myimage'), {
+    cropper = new Cropper(el('myimage'), {
 			zoomOnWheel: false,
-			built: function() {
-				this.cropper.scaleX(scale);
-				this.cropper.scaleY(scale);
-				this.cropper.zoomTo(1);
+      viewMode: 1,
+      movable: false,
+      rotatable: false,
+			ready: function() {
+
+        var canvasData = this.cropper.getCanvasData();
+        var scale = 1;
+        if(image.naturalWidth > canvasData.width) {
+          scale = canvasData.width/image.naturalWidth;
+          image.width = canvasData.width;
+          this.cropper.scaleX(scale);
+          this.cropper.scaleY(scale);
+          this.cropper.zoomTo(1);
+        } else {
+          canvasData.width = image.width;
+          this.cropper.setCanvasData(canvasData);
+        }
+
 				free_mode_data(this.cropper);
 			},
       crop: function(data) {
-				document.getElementById('img-height').innerHTML = data.height;
-				document.getElementById('img-width').innerHTML = data.width;
+				el('img-height').innerHTML = Math.round(data.detail.height);
+				el('img-width').innerHTML = Math.round(data.detail.width);
       }
     });
 	};
@@ -93,13 +119,20 @@ function select_all() {
 function download_jpg() {
 	if(cropper != null) {
 		var canv = cropper.getCroppedCanvas();
-		window.open(canv.toDataURL("image/jpeg", 0.9));
+    download(canv.toDataURL("image/jpeg", 0.9), getFileName(cropper) + ".jpg", "image/jpeg");
+		// window.open(canv.toDataURL("image/jpeg", 0.9));
 	}
 }
 
 function download_png() {
 	if(cropper != null) {
 		var canv = cropper.getCroppedCanvas();
-		window.open(canv.toDataURL());
+    download(canv.toDataURL(), getFileName(cropper) + ".png", "image/png");
+		// window.open(canv.toDataURL());
 	}
+}
+
+function getFileName(cropper) {
+  var cropData = cropper.getCropBoxData();
+  return filename.split(".").slice(0, -1).join(".") + "-" + Math.round(cropData.width) + "x" + Math.round(cropData.height);
 }
