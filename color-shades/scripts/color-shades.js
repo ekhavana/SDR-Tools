e0e08c690f84a40b6bac06e2d5da9319b7a151b0
Travis-CI <kanishk.kunal@hotmail.com>
2016-10-29 07:31:56 +0000

    Updates

---
+++ color-shades/scripts/color-shades.js
@@ -0,0 +1,66 @@
+new Vue({
+  el: '#app',
+  data: {
+    color: '#fafafa',
+    selectedIndex: 4
+  },
+  computed: {
+    previewStyleIn: function () {
+      var color = this.color
+      if (this.color.length == 0) {
+        color = "#fafafa"
+      }
+
+      return "background-color: " + tinycolor(color).toRgbString() + ";"
+    },
+    previewStyleOut: function () {
+      return this.shadesMonochrome[this.selectedIndex]
+    },
+    colorOut: function () {
+      var color = tinycolor(this.shadesMonochrome[this.selectedIndex])
+      return {
+        hex: color.toHexString(),
+        rgb: color.toRgbString(),
+        hsl: color.toHslString()
+      }
+    },
+    shadesMonochrome: function () {
+      var shades = []
+      var color = this.color
+      if (this.color.length == 0) {
+        color = "#fafafa"
+      }
+      var hsl = tinycolor(color).toHsl()
+      for (var i = 9.5; i >= 0.5; i -= 1) {
+        hsl.l = 0.1 * i;
+        shades.push("background-color: " + tinycolor(hsl).toRgbString())
+      }
+      return shades
+    }
+  },
+  methods: {
+    copy: function (text, event) {
+      var selection = window.getSelection()
+      selection.removeAllRanges()
+
+      event.target.setSelectionRange(0,  event.target.value.length)
+
+      // Copy the selection to clipboard.
+      document.execCommand('copy')
+
+      swal({
+        title: "Copied!",
+        text: "<strong>" + text + "</strong>",
+        timer: 3000,
+        showConfirmButton: true,
+        html: true
+      });
+    },
+    random: function () {
+      this.color = tinycolor.random().toHexString()
+    }
+  },
+  created: function () {
+    this.random()
+  }
+})
