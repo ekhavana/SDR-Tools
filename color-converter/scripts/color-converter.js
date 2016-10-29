a161b8fd50031daa8054f6bd573f28f20f64d2ee
Travis-CI <kanishk.kunal@hotmail.com>
2016-10-29 11:44:25 +0000

    Updates

--- color-converter/scripts/color-converter.js
+++ color-converter/scripts/color-converter.js
@@ -1,8 +1,7 @@
 new Vue({
   el: '#app',
   data: {
-    color: '#fafafa',
-    selectedIndex: 4
+    color: '#fafafa'
   },
   computed: {
     previewStyleIn: function () {
@@ -17,26 +16,17 @@ new Vue({
       return this.shadesMonochrome[this.selectedIndex]
     },
     colorOut: function () {
-      var color = tinycolor(this.shadesMonochrome[this.selectedIndex])
+      var color = tinycolor(this.color)
+      if (this.color.length == 0) {
+        color = tinycolor("#fafafa")
+      }
+
       return {
         hex: color.toHexString(),
         rgb: color.toRgbString(),
         hsl: color.toHslString(),
         hsv: color.toHsvString()
       }
-    },
-    shadesMonochrome: function () {
-      var shades = []
-      var color = this.color
-      if (this.color.length == 0) {
-        color = "#fafafa"
-      }
-      var hsl = tinycolor(color).toHsl()
-      for (var i = 9.5; i >= 0.5; i -= 1) {
-        hsl.l = 0.1 * i;
-        shades.push("background-color: " + tinycolor(hsl).toRgbString())
-      }
-      return shades
     }
   },
   methods: {
